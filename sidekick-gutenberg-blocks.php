d69e60934b6c3dbad05221dcc6d76fb8179483c6
Josh Baker <josh@baker.dev>
2026-07-05 11:59:11 -0500

    proper version

--- sidekick-gutenberg-blocks.php
+++ sidekick-gutenberg-blocks.php
@@ -5,7 +5,7 @@
  * Description: A collection of custom Gutenberg blocks built by Josh Baker
  * Author: Josh Baker
  * Author URI: https://baker.dev/
- * Version: 1.0.3
+ * Version: 1.0.4
  *
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
